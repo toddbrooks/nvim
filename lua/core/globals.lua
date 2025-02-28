cdd524ea0ddfb8d481ca883fa4f5062156fd0901
Todd Brooks <todd.brooks@thecodefoundry.com>
2025-02-27 18:35:42 -0600

    New LazyVim configuration for Neovim

--- lua/core/globals.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.g.copilot_no_tab_map = true
-
--- lua/plugins/ui.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- file icons for file explorer
-  "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("nvim-web-devicons").setup({})
-    -- config here
-  end,
-}
